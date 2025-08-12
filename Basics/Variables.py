9e2656803e18a2949eeb2114a7e25f9bd2c9b701
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-12 22:44:36 +0530

    Noteables

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -7,5 +7,14 @@ print(a)
 b='Jotiraditya Hazra'
 
 print(type(b))
-
+\
 q=complex(2, 3)  # complex number with real part 2 and imaginary part 3
+
+
+l = [1, 2, 3, 4, 5] #mutable
+tuple = (1, 2, 3, 4, 5) #immutable 
+
+dic = {'name': 'Jotiraditya', 'age': 20, 'is_student': True} #dictionary mapped values 
+
+
+# every thing in python is an object 
