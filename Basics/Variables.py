f77c99c9f9ee8cf5eaff398bac3229642ed460ee
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-12 22:36:12 +0530

    Variables 1

---
+++ Basics/Variables.py
@@ -0,0 +1,11 @@
+# variables are containers 
+# saying to the computer to store that value in RAM 
+
+a=235759723957259
+print(a)
+
+b='Jotiraditya Hazra'
+
+print(type(b))
+
+q=complex(2, 3)  # complex number with real part 2 and imaginary part 3
