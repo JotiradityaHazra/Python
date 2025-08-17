5ccbc100190d6cd1b08e0d66cc1dabbabb80423a
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-17 22:37:23 +0530

    USer_Input

---
+++ Basics/USer_Input.py
@@ -0,0 +1,10 @@
+
+# User Input Example
+a= input("Enter your Name ")  #default enter as a string u need to type if u want a desired data type
+print(a)
+
+x=int(input("Enter a number: "))  #converting input to integer
+y=int(input("Enter another number: "))
+
+print(x + y)  # Output: sum of the two integers
+
