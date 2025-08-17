a98e6df7dc3b7a240c1e2d0afa2ed1a33c0cfa29
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-17 23:50:37 +0530

    String 1

---
+++ Basics/Strings.py
@@ -0,0 +1,16 @@
+# any thing under quation is a string (double single doesnt matter)
+# string is like (not a array) a array of characters
+
+# /n next line 
+
+print("hello \n world")
+
+# Indexing 
+
+a ='hello'
+
+print (a[4]) 
+
+# continue indexing in the loop segement 
+
+
