f37a442177575fe9d41f9a7b532907ba71db131a
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-17 21:21:38 +0530

    Type_casting

---
+++ Basics/Type_casting.py
@@ -0,0 +1,19 @@
+a="1"
+b='2'
+
+print (a + b)  # Output: 3
+
+
+
+
+# Type casting is the process of converting one data type to another
+
+print(int(a) + int(b))   #string to integer conversion
+
+# explicit_casting => dev does it 
+# implicit_casting => python does it automatically
+
+c=3.6
+d=6
+
+print(c + d)    # Output: 9 (float to integer conversion) implemented implicitly
