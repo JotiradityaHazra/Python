107bd01a5571800f635f03b24b8c9daec26eb064
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-18 23:02:46 +0530

    String 2

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -11,6 +11,21 @@ a ='hello'
 
 print (a[4]) 
 
-# continue indexing in the loop segement 
+# continue indexing in the loop segment 
 
+# String Slicing 
 
+name = "Jotiraditya Hazra"
+
+print(name[0:7]) # begin with 0 end at 6 (not 7)  [:x] means from 0 to x-1
+
+print(a[:-2]) # means from 0 to len(a)-3 
+
+fruit = input("Enter fruit: ")
+
+lenfru =len(fruit)
+
+print("Your fruit has",lenfru ,"letters")
+
+b="popcorn"
+print(b[-4:-2]) 
