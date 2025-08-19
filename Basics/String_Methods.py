aafa22bfeb78035f595778a0ac9529e4108d2b44
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-19 22:03:36 +0530

    String_Methods.py changes

--- Basics/String_Methods.py
+++ Basics/String_Methods.py
@@ -4,9 +4,21 @@ print(len(a))   # length of string
 print(a.upper())  # convert to uppercase
 print(a.lower())  # convert to lowercase
 
-print(a.strip("a"))  # remove leading and trailing 'a'
+print(a.strip("a")) # remove leading and trailing 'a'
+print(a.rstrip("a"))   # remove trailing 'a'
 
 print(a.replace("Jotiraditya", "Joti"))  # replace substring 
 
+print(a.split("i"))  # split string by 'i' creating a list split from the character 'i'
 
 
+blog ="intro to Python"
+
+print(blog.capitalize()) # capitalize the first letter of the string
+
+print(blog.title())  # capitalize the first letter of each word
+
+print(blog.count("o"))  # count occurrences of 'o'
+
+
+# there are mutiple methods of string so practice and play around with strings and as you use them u will be more familiar with them 
