3d4ff130ef3cc659c21331f9e7c4c62f5ec0fe94
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-19 21:24:29 +0530

    String _Methods Update

---
+++ Basics/String_Methods.py
@@ -0,0 +1,12 @@
+a= "Jotiraditya"
+print(len(a))   # length of string
+
+print(a.upper())  # convert to uppercase
+print(a.lower())  # convert to lowercase
+
+print(a.strip("a"))  # remove leading and trailing 'a'
+
+print(a.replace("Jotiraditya", "Joti"))  # replace substring 
+
+
+
