48dad5c49c2b7283f058ce896b7ca048ce045de6
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-20 15:52:23 +0530

    if else extension

--- Loops/if_else.py
+++ Loops/if_else.py
@@ -1,6 +1,9 @@
 # Loops are used in python to execute decision making statement 
 # here we are going to learn about if else loop 
 
+#conditional operators 
+# >=, <, >, <=, >=, ==, != 
+
 a =int(input("Enter your age "))
 
 print("Your Age is ",a)
@@ -9,3 +12,14 @@ if a>18:
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
+    
+    
+budget = 200
+    
+Expense =190 
+    
+if Expense>=budget:
+        print("Gareeb")
+else: 
+        print("Thik hai")
+        
