1c7dbe6d15f31b08578d7f7ae5b2a2cdf4b509e3
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-20 15:39:11 +0530

    simple if else

---
+++ Loops/if_else.py
@@ -0,0 +1,11 @@
+# Loops are used in python to execute decision making statement 
+# here we are going to learn about if else loop 
+
+a =int(input("Enter your age "))
+
+print("Your Age is ",a)
+
+if a>18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
