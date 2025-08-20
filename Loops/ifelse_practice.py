a5e9391b49246f4db1b72e292bc8eff8fa35c75c
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-20 20:58:17 +0530

    Simple_Practice_ifelse

--- Loops/ifelse_practice.py
+++ Loops/ifelse_practice.py
@@ -5,3 +5,13 @@ print(timestamp)
 
 # your program should greet you Good morning, good afternoon, or good evening based on the current time
 
+time=int(time.strftime("%H")) 
+
+print(time)
+
+if time <12 :
+    print("Good Morning")
+elif time <= 17:
+    print("Good Afternoon")
+else:
+    print("Good Evening")   
