1c287bc31b807da0bf29703cb14ff1e8706ab8b3
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-20 17:16:39 +0530

    practice_ifelse

---
+++ Loops/ifelse_practice.py
@@ -0,0 +1,7 @@
+import time 
+timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+print(timestamp)
+
+# your program should greet you Good morning, good afternoon, or good evening based on the current time
+
