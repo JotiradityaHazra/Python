0ca9ba8a1d5d34f101bcd41f3913a6218ce4e5ef
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-08-21 20:19:13 +0530

    For Loop concepts

---
+++ Loops/For_Loop/For_loop.py
@@ -0,0 +1,25 @@
+# For loop is used to iterate over a sequence (like a list, tuple, dictionary, set, or string) or other iterable objects.
+
+# for i in range(20):
+#     print(i)
+    
+    
+name = "John"
+for j in name :
+    print(j)
+    if j == "o":
+        print("Found an 'o' in the name!")
+
+colors = ["red", "green", "blue"]     #print the elements of the list 
+for color in colors:
+    print (f"color: {color}")
+    
+    
+    
+# Range function is used to generate a sequence of numbers.
+
+for k in range(5,10,2):  # start at 5, end before 10, step by 2 
+    print(k+1)
+    
+    
+    
