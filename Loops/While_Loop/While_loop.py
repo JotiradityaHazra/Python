2758ca8bde25ee3b281b2e6be868d4465f226fad
Jotiraditya <jotiraditya.hajra@gmail.com>
2025-09-05 23:37:49 +0530

    While update

--- Loops/While_Loop/While_loop.py
+++ Loops/While_Loop/While_loop.py
@@ -6,5 +6,9 @@ while(i<3):
     i =  i+1
     
     
-for j in range(1,100,20):
-    print(j-1)   
+# for j in range(1,100,20):
+#     print(j-1)   
+while(i<10):
+        i= int(input("Enter a number "))
+        print(i)
+        print("Done")      
