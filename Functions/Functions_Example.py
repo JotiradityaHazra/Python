4413b240d6ed680fa9af188714fd1f2c009dad99
Jotiraditya <jotiraditya.hajra@gmail.com>
2026-01-16 10:20:46 +0530

    practice_questionsfunctions_1

---
+++ Functions/Functions_Example.py
@@ -0,0 +1,22 @@
+ 
+
+# Q1 - Write a function to greet the user
+
+def greet():
+    print("hello")
+
+greet()
+
+# Q2 - write a function greet the user 3 three times
+
+for i in range(3):
+    greet()
+
+
+# Q3 - write a function that returns the square of a number
+
+num = int(input("Enter a number: "))
+def square(num):
+    return num * num
+
+print(square(num))
